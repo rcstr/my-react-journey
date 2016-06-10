57e81a2c3c1e4ddba60d989352f86a62605b08cf
Rommel Castro <rommelxcastro@gmail.com>
2016-06-10 09:36:43 -0600

    hashHistory typo

--- app/app.jsx
+++ app/app.jsx
@@ -2,12 +2,12 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Route, Router, IndexRoute, hasHistory} from 'react-router';
+import {Route, Router, IndexRoute, hashHistory} from 'react-router';
 
 import Main from './components/Main';
 
 ReactDOM.render(
-    <Router history={hasHistory}>
+    <Router history={hashHistory}>
         <Route path="/" component={Main}/>
     </Router>,
     document.getElementById('app')
